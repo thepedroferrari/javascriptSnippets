27f55b5318b3a7c0e1123e4bc2f9f2d573e3d841
Pedro Ferrari <pedro@pedroferrari.com>
2018-06-25 13:44:48 +0200

    add GlassDoor Filter

---
+++ glassdoor-company-filter.js
@@ -0,0 +1,13 @@
+// GlassDoor filter lower Rating Companies
+
+const desiredRating = 3.5; // Choose your rating from 0.0 to 5.0
+
+const companies = document.querySelectorAll('.eiHdrModule');
+const companiesArray = Array.from(companies)
+
+for (const company of companiesArray) {
+    const findRating = company.querySelector('.bigRating');
+    if (findRating.innerText < desiredRating) {
+        company.style.display = 'none';
+	}
+}
