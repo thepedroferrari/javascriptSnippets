21d3afda4031968979ff4b1bef9fc8c3ef9347e8
Pedro Ferrari <pedro@pedroferrari.com>
2018-07-26 17:16:07 +0200

    Update konami.js

--- konami.js
+++ konami.js
@@ -5,7 +5,7 @@ window.addEventListener('keyup', (e) => {
   pressed.push(e.key);
   pressed.splice(-secretCode.length - 1, pressed.length - secretCode.length);
   if (pressed.join('').includes(secretCode)) {
-		const imgs = Array.from(document.getElementsByTagName("img"));
+	const imgs = Array.from(document.getElementsByTagName("img"));
 		imgs.map(img => {
 			let ts = new Date().getTime();
 			img.src = "https://thecatapi.com/api/images/get?format=src&type=gif&timestamp="+ts;
