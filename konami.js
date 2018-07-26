cb643d1fd8eee29e3f21e6e0f2c53de885cf3306
Pedro Ferrari <pedro@pedroferrari.com>
2018-07-26 17:15:26 +0200

    Create konami.js

---
+++ konami.js
@@ -0,0 +1,14 @@
+// Write my name to change all images to cat gifs
+const pressed = [];
+const secretCode = 'Pedro';
+window.addEventListener('keyup', (e) => {
+  pressed.push(e.key);
+  pressed.splice(-secretCode.length - 1, pressed.length - secretCode.length);
+  if (pressed.join('').includes(secretCode)) {
+		const imgs = Array.from(document.getElementsByTagName("img"));
+		imgs.map(img => {
+			let ts = new Date().getTime();
+			img.src = "https://thecatapi.com/api/images/get?format=src&type=gif&timestamp="+ts;
+		})
+	}
+});
